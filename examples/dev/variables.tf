863767cfd8ab0edc7111ff2f4dcd892c69fecb18
ppresto <ppresto@hashicorp.com>
2019-09-04 16:12:23 -0700

    init

--- examples/dev/variables.tf
+++ examples/dev/variables.tf
@@ -1,8 +1,8 @@
 variable "name_prefix"       { }
 variable "vpc_cidrs_public"  { type = "list" default = ["10.139.1.0/24",]}
 variable "vpc_cidrs_private" { type = "list" default = ["10.139.11.0/24",]}
-variable "nat_count"         { default = 1}
-variable "bastion_count"     { default = 1}
+variable "nat_count"         { default = "1"}
+variable "bastion_count"     { default = "1"}
 
 variable "tags" {
   type        = "map"
