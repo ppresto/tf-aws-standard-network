c5855a941b3d80b33683402c5e0ab840bf78d64c
ppresto <ppresto@hashicorp.com>
2019-09-05 11:27:34 -0700

    default dev name prefix

--- examples/dev/variables.tf
+++ examples/dev/variables.tf
@@ -1,4 +1,5 @@
+name    =   "ppresto-dev-std-network"
 vpc_cidrs_public = ["10.139.1.0/24",]
 vpc_cidrs_private = ["10.139.11.0/24",]
-nat_count  = "1"
-bastion_count  = "1"
+nat_count  = 1
+bastion_count  = 1
