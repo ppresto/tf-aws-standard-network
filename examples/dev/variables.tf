7fc612af41b77f40170aa2d6d5a4b70a8d54e054
ppresto <ppresto@hashicorp.com>
2019-09-05 09:39:05 -0700

    dev variables defined

--- examples/dev/variables.tf
+++ examples/dev/variables.tf
@@ -1,10 +1,4 @@
-variable "name_prefix"       { }
-variable "vpc_cidrs_public"  { type = "list" default = ["10.139.1.0/24",]}
-variable "vpc_cidrs_private" { type = "list" default = ["10.139.11.0/24",]}
-variable "nat_count"         { default = "1"}
-variable "bastion_count"     { default = "1"}
-
-variable "tags" {
-  type        = "map"
-  default     = {}
-}
+vpc_cidrs_public = ["10.139.1.0/24",]
+vpc_cidrs_private = ["10.139.11.0/24",]
+nat_count  = "1"
+bastion_count"  = "1"
